21669f5a25a34200d5116dd8da2d485f767c2047
agakoz <agataakoziol@gmail.com>
2020-03-26 11:11:57 +0100

    method returning name of a month with working test implemented

--- src/main/java/pl/accenture/szkolka/pp/Krok2.java
+++ src/main/java/pl/accenture/szkolka/pp/Krok2.java
@@ -1,14 +1,53 @@
 package pl.accenture.szkolka.pp;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Krok2 {
+    private static final Logger LOG = Logger.getLogger("pl.accenture.szkolka.pp.Krok2");
+
     // Wszystkie implementacje - razem z testami JUnit
-// TODO 1 zaimplementuj metodę z uzyciem if
+// Done 1 zaimplementuj metodę z uzyciem if
     public static boolean checkIfAdult(int age) {
         if (age >= 18) return true;
             else return false;
 
         }
 // TODO 2 zaimplementuj metodę z uzyciem switch
+
+    public static String returnMonthName(int monthNum){
+        switch (monthNum){
+            case 1: return "january";
+
+            case 2: return "february";
+
+            case 3: return "march";
+
+            case 4: return "april";
+
+            case 5: return "may";
+
+            case 6: return "june";
+
+            case 7: return "july";
+
+            case 8: return"august";
+
+            case 9: return"september";
+
+            case 10: return "october";
+
+            case 11: return "november";
+
+            case 12: return "december";
+
+            default: return "error";
+
+
+        }
+    }
+
+
 // TODO 2 zaimplementuj metodę z uzyciem (do) while
 // TODO 3 zaimplementuj metodę z uzyciem for i break
 // TODO 4 zaimplementuj metodę z uzyciem foreach i continue
--- src/test/java/pl/accenture/szkolka/pp/Krok2Test.java
+++ src/test/java/pl/accenture/szkolka/pp/Krok2Test.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Krok2Test {
     @Test
     @DisplayName("18 -> true")
-    void checkIfAdultTrue(){
+    void checkIfAdultTrue() {
         //given
         int age = 18;
         //when age of the adulthood starts from 18
@@ -18,11 +18,23 @@ class Krok2Test {
 
     @Test
     @DisplayName("17 -> false")
-    void checkIfAdultFalse(){
+    void checkIfAdultFalse() {
         //given
         int age = 17;
         //when age of the adulthood starts from 18
         //then
         assertFalse(Krok2.checkIfAdult(age));
+    }
+
+    @Test
+    @DisplayName("1 -> january")
+    void returnMonthName() {
+        //given
+        int monthNum = 1;
+        String expectedName = "january";
+                //when
+        String returnedName = Krok2.returnMonthName(monthNum);
+        //then
+        assertEquals(expectedName, returnedName);
     }
 }
